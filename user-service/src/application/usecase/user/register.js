d0f07beab3697c4603bb8b52ddaca3909311218c
officialrashid <muhammedrashi59@gmail.com>
2023-08-03 14:18:37 +0530

    user success addded

--- user-service/src/application/usecase/user/register.js
+++ user-service/src/application/usecase/user/register.js
@@ -11,7 +11,7 @@ const registerUser = async (name, email, password, repository, authService) => {
             const changePassword = await authService.bycriptPassword(password);//passwod bcrypt function
             const userDetails = userdata(name, email, changePassword); // Pass the hashed password instead of the original password.
             const createUser = await repository.createUser(userDetails);//call the userRepositoryInf inside the createUser fuction.
-            return ({status:true,createUser})
+            return ({status:true,createUser,success:true})
         }else{
             return {message:"email already exist in the databse"}
         }
