8503895c0ce934157bd9f449ee605d5c5f8c65db
officialrashid <muhammedrashi59@gmail.com>
2023-08-16 02:19:26 +0530

    again set true

--- product-service/src/application/usecase/product/getProducts.js
+++ product-service/src/application/usecase/product/getProducts.js
@@ -3,7 +3,7 @@ const getProducts = async (repositories, productService) => {
       const response = await repositories.getProducts();
       console.log(response, "Response from getProducts repositories");
   
-      return { status: false, products: response };
+      return { status: true, products: response };
     } catch (error) {
       console.error("Error in getProducts use case:", error);
       throw error;
