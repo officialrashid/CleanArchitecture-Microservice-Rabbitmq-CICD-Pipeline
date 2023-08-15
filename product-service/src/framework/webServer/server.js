e4494ac212db815ef75fa00def2c119d3ee39b29
officialrashid <muhammedrashi59@gmail.com>
2023-08-16 00:28:28 +0530

    changes the urls

--- product-service/src/framework/webServer/server.js
+++ product-service/src/framework/webServer/server.js
@@ -2,6 +2,7 @@ import "../../rabbitmqConsume/consumer.js"
 const serverConfig = (server, config) => {
   const startServer = () => {
     server.listen(config.port, () => {
+      
       console.log(`Server listening on port ${config.port} product-service`);
     });
   };
--- user-service/src/framework/webServer/server.js
+++ user-service/src/framework/webServer/server.js
@@ -1,7 +1,10 @@
 // serverConfig.js
 const serverConfig = (server, config) => {
+
     const startServer = () => {
+      
       server.listen(config.port, () => {
+
         console.log(`Server listening on port 3000 - userService`);
       });
     };
