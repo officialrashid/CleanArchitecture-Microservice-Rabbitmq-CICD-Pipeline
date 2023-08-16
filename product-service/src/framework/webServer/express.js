d10b1cf5f5cce4019686d3ac373a81724fca2266
officialrashid <muhammedrashi59@gmail.com>
2023-08-16 23:19:27 +0530

    chnage the url

--- product-service/src/framework/webServer/express.js
+++ product-service/src/framework/webServer/express.js
@@ -6,7 +6,11 @@ const expressConfig = (app) => {
   app.use(morgan('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(cors({
+    origin: 'https://nextjs-project-57233.web.app', // Update with your client's origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true // Allow credentials (cookies, etc.)
+}));
 
 };
 
