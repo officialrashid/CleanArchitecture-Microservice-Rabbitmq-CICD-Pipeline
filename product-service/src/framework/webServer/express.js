a620d1e01e02f26b5b99dda266099092ccf63990
officialrashid <muhammedrashi59@gmail.com>
2023-08-17 00:01:17 +0530

    change the urls

--- product-service/src/framework/webServer/express.js
+++ product-service/src/framework/webServer/express.js
@@ -7,7 +7,7 @@ const expressConfig = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors({
-    origin: 'https://nextjs-project-57233.web.app', // Update with your client's origin
+    origin: 'https://fascinating-cactus-2a9b9e.netlify.app/', // Update with your client's origin
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true // Allow credentials (cookies, etc.)
 }));
