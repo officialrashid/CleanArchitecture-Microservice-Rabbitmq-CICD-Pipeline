617f8041a99636e01d4762f8b65bd2d22e08e275
officialrashid <muhammedrashi59@gmail.com>
2023-08-17 13:39:22 +0530

    cors issue done

--- product-service/src/framework/webServer/express.js
+++ product-service/src/framework/webServer/express.js
@@ -6,11 +6,7 @@ const expressConfig = (app) => {
   app.use(morgan('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors({
-    origin: 'https://nextjs-project-81ff4.web.app', // Update with your client's origin
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true // Allow credentials (cookies, etc.)
-}));
+  app.use(cors());
 
 };
 
